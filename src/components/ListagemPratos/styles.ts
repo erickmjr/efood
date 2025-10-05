cb14bac211a339aab7e77ab7d55da3ee7a2535f6
erickmjr <erickmellojr@gmail.com>
2025-10-05 12:58:17 -0300

    fix(styles): spacing

--- src/components/ListagemPratos/styles.ts
+++ src/components/ListagemPratos/styles.ts
@@ -3,7 +3,8 @@ import styled from 'styled-components';
 export const Listagem = styled.section`
     display: grid;
     grid-template-columns: 1fr 1fr 1fr;
-    grid-row-gap: 32px;
+    gap: 32px;
     padding: 56px 171px 120px 171px;
     margin: 0 auto;
+    width: fit-content;
 `;
